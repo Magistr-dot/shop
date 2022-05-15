83fbd9d21d766bba4a82979de8a30e8e2c5efdc6
Alex <nesterov178@gmail.com>
2022-05-15 12:24:21 +0300

    UserActivity

---
+++ app/src/main/java/com/example/shop/presentation/UserActivity.kt
@@ -0,0 +1,21 @@
+package com.example.shop.presentation
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shop.R
+
+class UserActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
